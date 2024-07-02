d9f8fcf9cb701ef49285eefa548cb2cb0ba829fd
luca <lfbianchini@dons.usfca.edu>
2024-07-01 21:27:40 -0700

    added directory creation for year and date

--- src/main/java/org/projects/Browser.java
+++ src/main/java/org/projects/Browser.java
@@ -3,49 +3,48 @@ package org.projects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import java.util.Arrays;
 
 public class Browser {
     private final WebDriver driver;
     private String url;
-    private String parentDirectory;
+    private DirectoryTool dTool;
 
     public Browser(String url, String parentDirectory) {
         this.url = url;
-        this.parentDirectory = parentDirectory;
+        dTool = new DirectoryTool(parentDirectory);
         driver = new ChromeDriver();
         checkIfValidUrl();
     }
 
     public void downloadPagesToDayDirectory() {
-        String year = "2024";
-        createYearDirectory(year);
+        //e.g. "The Daily Californian, Fri Jan. 3, 1975"
+        String title = driver.findElement(By.cssSelector("div.metadata-row:nth-child(1) > span:nth-child(2)")).getText();
+        String[] parts = title.split(",\\s");
+        String date = parts[1].substring(4);
+        String year = parts[2];
+
+        createDirectory(year, date);
+
+
     }
 
-    private void createYearDirectory(String year) {
-        String yearDirectoryPath = parentDirectory + File.separator + year;
-        File yearDirectory = new File(yearDirectoryPath);
-
-        if (!yearDirectory.exists()) {
-            boolean created = yearDirectory.mkdir();
-            if (created) {
-                System.out.println("Directory created: " + yearDirectory.getAbsolutePath());
-            } else {
-                throw new RuntimeException("Failed to create directory: " + yearDirectory.getAbsolutePath());
-            }
-        } else {
-            System.out.println("Directory for year " + year + " already exists..  " + yearDirectory.getAbsolutePath());
-        }
+    private void createDirectory(String year, String date) {
+        dTool.createYearDirectory(year);
+        dTool.createDateDirectory(date);
     }
+
+
 
     private void checkIfValidUrl() {
+        System.out.println("Checking if URL is valid");
         driver.get(url);
         try {
-            WebElement fullCollectionName = driver.findElement(By.cssSelector("div.metadata-row:nth-child(4) > span:nth-child(2)"));
-            if(!fullCollectionName.getText().equals("The Daily Californian")) {
+            String fullCollectionName = driver.findElement(By.cssSelector("div.metadata-row:nth-child(4) > span:nth-child(2)")).getText();
+            if(!fullCollectionName.equals("The Daily Californian")) {
                 throw new NoSuchElementException("Not a The Daily Californian article");
             }
         } catch (NoSuchElementException e) {
@@ -54,13 +53,6 @@ public class Browser {
         }
     }
 
-    public String getParentDirectory() {
-        return parentDirectory;
-    }
-
-    public void setParentDirectory(String parentDirectory) {
-        this.parentDirectory = parentDirectory;
-    }
 
     public String getUrl() {
         return url;
--- src/main/java/org/projects/DirectoryTool.java
+++ src/main/java/org/projects/DirectoryTool.java
@@ -1,7 +1,10 @@
 package org.projects;
 
+import java.io.File;
+
 public class DirectoryTool {
     private final String parentDirectory;
+    private String currentDirectory;
 
     public DirectoryTool(String parentDirectory) {
         this.parentDirectory = parentDirectory;
@@ -9,5 +12,45 @@ public class DirectoryTool {
 
     public String getParentDirectory() {
         return parentDirectory;
+    }
+
+    public void createDirectory(String year, String date) {
+        createYearDirectory(year);
+        createDateDirectory(date);
+    }
+
+    public void createDateDirectory(String date) {
+        String dateDirectoryPath = currentDirectory + File.separator + date;
+        File dateDirectory = new File(dateDirectoryPath);
+
+        if(!dateDirectory.exists()) {
+            boolean created = dateDirectory.mkdir();
+            if(created) {
+                System.out.println("Directory created: " + dateDirectoryPath);
+                currentDirectory = dateDirectoryPath;
+            } else {
+                throw new RuntimeException("Failed to create directory: " + dateDirectory.getAbsolutePath());
+            }
+        } else {
+            System.out.println("Directory for date " + date + " already exists, using this directory  ");
+            currentDirectory = dateDirectoryPath;
+        }
+    }
+    public void createYearDirectory(String year) {
+        String yearDirectoryPath = parentDirectory + File.separator + year;
+        File yearDirectory = new File(yearDirectoryPath);
+
+        if(!yearDirectory.exists()) {
+            boolean created = yearDirectory.mkdir();
+            if(created) {
+                System.out.println("Directory created: " + yearDirectory.getAbsolutePath());
+                currentDirectory = yearDirectoryPath;
+            } else {
+                throw new RuntimeException("Failed to create directory: " + yearDirectory.getAbsolutePath());
+            }
+        } else {
+            System.out.println("Directory for year " + year + " already exists..  " + yearDirectory.getAbsolutePath());
+            currentDirectory = yearDirectoryPath;
+        }
     }
 }
